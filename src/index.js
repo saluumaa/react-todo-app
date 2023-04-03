84b92144d24d6899d5eeb6d90fa68aeea1d5c05f
saluumaa <salumahassan02@gmail.com>
2023-04-03 08:39:25 +0300

    Add todo functionality

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './app.css';
+import ReactDOM from "react-dom/client";
+import TodoApp from './components/TodoApp';
+import { BrowserRouter as Router } from "react-router-dom"
+
+const domContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <TodoApp />
+    </Router>
+  </React.StrictMode>
+);
+
